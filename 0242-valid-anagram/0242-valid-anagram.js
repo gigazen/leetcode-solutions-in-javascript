be96e7e148ff10376eb1b4a6fb8d28b3195b792d
gigazen <debanjanb01234@gmail.com>
2023-04-30 19:43:45 +0530

    Time: 109 ms (14.54%), Space: 48 MB (25.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,31 +3,14 @@
  * @param {string} t
  * @return {boolean}
  */
-let isAnagram = function (s, t) {
-  if (s.length !== t.length) return false;
-
-  let [countS, countT] = [{}, {}];
-
-  for (let i = 0; i < s.length; i++) {
-    if (!countS[s[i]]) {
-      countS[s[i]] = 1;
-    } else {
-      countS[s[i]] = countS[s[i]] + 1;
+var isAnagram = function(s, t) {
+    let sortedS = [...s].sort().join("");
+    let sortedT = [...t].sort().join("");
+    
+    if (sortedS === sortedT) {
+        return true;
     }
-  }
-
-  for (let i = 0; i < t.length; i++) {
-    if (!countT[t[i]]) {
-      countT[t[i]] = 1;
-    } else {
-      countT[t[i]] = countT[t[i]] + 1;
-    }
-  }
-
-  for (let key in countS) {
-    if (countS[key] !== countT[key]) return false;
-  }
-
-  return true;
+    
+    return false;
 };
 
