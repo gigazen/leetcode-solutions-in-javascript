7289d3a474d902803a6e887ddab89ea783f79262
realdeb <debanjanb01234@gmail.com>
2023-08-16 18:20:06 +0530

    Time: 77 ms (66.94%), Space: 43.7 MB (67.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,39 +4,27 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-  // if any of the strings is not defined or falsy then not anagram, if their lengths are not same then also not anagram   
-  if (!s || !t) return false;
-  if (s.length !== t.length) return false;
-
-  const charCountMap = new Map();
-  
-  // iterate through the first string and create character-count, key-value entries in map 
-  for (let i = 0; i < s.length; i++) {
-    const currChar = s.charAt(i);
-
-    if (!charCountMap.has(currChar)) {
-      charCountMap.set(currChar, 1);
-    } else {
-      let charCount = charCountMap.get(currChar);
-      charCountMap.set(currChar, charCount + 1);
+    if (!s || !t) return false;
+    if (s.length !== t.length) return false;
+    
+    const map = new Map();
+    
+    for (let char of s) {
+        if (!map.has(char)) {
+            map.set(char, 1);
+        } else {
+            map.set(char, map.get(char) + 1);
+        }
     }
-  }
-
-  // iterate through the second string and delete character-count, key-value entries from map, if matched  
-  for (let i = 0; i < t.length; i++) {
-    const currChar = t.charAt(i);
-
-    if (!charCountMap.has(currChar)) {
-      return false;
+    
+    for (let char of t) {
+        if (!map.has(char)) return false;
+        if (map.get(char) === 1) {
+            map.delete(char);
+        } else {
+            map.set(char, map.get(char) - 1);
+        }
     }
-
-    if (charCountMap.get(currChar) === 1) {
-      charCountMap.delete(currChar);
-    } else {
-      let charCount = charCountMap.get(currChar);
-      charCountMap.set(currChar, charCount - 1);
-    }
-  }
-
-  return charCountMap.size === 0;
+    
+    return map.size === 0;
 };
