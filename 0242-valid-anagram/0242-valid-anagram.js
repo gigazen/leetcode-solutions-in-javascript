083bd5514c4de8c5ca2170d60a4fb9df250f66b5
realdeb <debanjanb01234@gmail.com>
2023-08-16 17:56:34 +0530

    Time: 54 ms (98.84%), Space: 41.8 MB (98.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,39 +3,40 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = (s, t, map = new Map()) => {
-    const isEqual = s.length === t.length;
-    if (!isEqual) return false;
+var isAnagram = function (s, t) {
+  // if any of the strings is not defined or falsy then not anagram, if their lengths are not same then also not anagram   
+  if (!s || !t) return false;
+  if (s.length !== t.length) return false;
 
-    addFrequency(s, map);      /* Time O(N) | Space O(1) */
-    subtractFrequency(t, map); /* Time O(N) | Space O(1) */
+  const charCountMap = new Map();
+  
+  // iterate through the first string and create character-count, key-value entries in map 
+  for (let i = 0; i < s.length; i++) {
+    const currChar = s.charAt(i);
 
-    return checkFrequency(map);/* Time O(N) */
+    if (!charCountMap.has(currChar)) {
+      charCountMap.set(currChar, 1);
+    } else {
+      let charCount = charCountMap.get(currChar);
+      charCountMap.set(currChar, charCount + 1);
+    }
+  }
+
+  // iterate through the second string and delete character-count, key-value entries from map, if matched  
+  for (let i = 0; i < t.length; i++) {
+    const currChar = t.charAt(i);
+
+    if (!charCountMap.has(currChar)) {
+      return false;
+    }
+
+    if (charCountMap.get(currChar) === 1) {
+      charCountMap.delete(currChar);
+    } else {
+      let charCount = charCountMap.get(currChar);
+      charCountMap.set(currChar, charCount - 1);
+    }
+  }
+
+  return charCountMap.size === 0;
 };
-
-const addFrequency = (str, map) => {
-    for (const char of str) {/* Time O(N) */
-        const count = (map.get(char) || 0) + 1;
-
-        map.set(char, count);   /* Space O(1) */
-    }
-}
-
-const subtractFrequency = (str, map) => {
-    for (const char of str) {/* Time O(N) */
-        if (!map.has(char)) continue;
-
-        const count = map.get(char) - 1;
-
-        map.set(char, count);   /* Space O(1) */
-    }
-};
-
-const checkFrequency = (map) => {
-    for (const [ char, count ] of map) {/* Time O(N) */
-        const isEmpty = count === 0;
-        if (!isEmpty) return false;
-    }
-
-    return true;
-}
