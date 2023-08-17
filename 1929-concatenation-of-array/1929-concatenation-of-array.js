d93cd2181118cd23dc3d1598f2a4de93b2907b07
realdeb <debanjanb01234@gmail.com>
2023-08-17 09:22:45 +0530

    Time: 78 ms (39.19%), Space: 46.5 MB (9.55%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,11 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function (nums) {
-  let ans = [];
-
-  for (let i = 0; i < nums.length * 2; i++) {
-    ans[i] = nums[i % nums.length];
-  }
-  
-  return ans;
+    return [...nums, ...nums];
 };
