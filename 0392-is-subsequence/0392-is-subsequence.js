0a205c2e803a92b146708786c43182258cc243d9
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-08-18 16:56:23 +0530

    Time: 62 ms (36.11%), Space: 41.7 MB (80.48%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -4,15 +4,15 @@
  * @return {boolean}
  */
 var isSubsequence = function (s, t) {
-  if (s === t) return true;
   if (s.length === 0) return true;
   if (s.length > t.length) return false;
 
-  let startIndex = 0;
-  for (let i = 0; i < s.length; i++) {
-    let indexOfCharInT = t.indexOf(s[i], startIndex);
-    if (indexOfCharInT === -1) return false;
-    startIndex = indexOfCharInT + 1;
+  let indexS = 0;
+  for (let i = 0; i < t.length; i++) {
+    if (s[indexS] === t[i]) {
+      indexS++;
+    }
   }
-  return true;
+
+  return indexS === s.length;
 };
