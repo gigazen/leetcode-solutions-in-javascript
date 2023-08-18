e5791b626390074953533e6eb0d750577b63d40c
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-08-18 13:29:01 +0530

    Time: 51 ms (84.97%), Space: 43.5 MB (7.20%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -3,24 +3,14 @@
  * @param {string} t
  * @return {boolean}
  */
-var isSubsequence = function(s, t) {
+function isSubsequence(s, t) {
+  const sChars = s.split('')
 
-    let count=0
-    let stack=0
-    for(let j=0;j<s.length;j++){
-        for(let i=count;i<t.length;i++){
-            if(s[j]===t[i]){    
-                count++
-                stack++
-                break
-            }
-            count++
-        }
+  for (const chr of t) {
+    if (sChars[0] === chr) {
+      sChars.shift();
     }
-    if(stack===s.length){
-        return true
-    }else{
-        return false
-    }
+  }
 
-};
+  return sChars.length === 0
+}
