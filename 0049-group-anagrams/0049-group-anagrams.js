3512683732069bc24821e5781a3fe8d13e524c33
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-08-23 20:34:30 +0530

    Time: 117 ms (67.25%), Space: 55.4 MB (18.38%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -8,13 +8,11 @@ var groupAnagrams = function (strs) {
 
   for (let str of strs) {
     let sortedStr = str.split("").sort().join("");
-    // ["aet","aet","ant","aet","ant","abt"]
 
     if (!obj[sortedStr]) {
       obj[sortedStr] = [];
     }
     obj[sortedStr].push(str);
-    // {"aet": ["eat", "tea", "ate"], "ant": ["tan", "nat"], "abt": ["bat"]}
   }
 
   return Object.values(obj);
