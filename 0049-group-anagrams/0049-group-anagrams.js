a625b3a6e1990344b2769844817e981d0e82050c
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-08-23 20:29:40 +0530

    Time: 102 ms (90.80%), Space: 54.1 MB (40.88%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,16 +3,13 @@
  * @return {string[][]}
  */
 // Using combination of Sorting and HashMap
-var groupAnagrams = function (strs) {
-  let obj = {};
+var groupAnagrams = function(strs) {
+    let map = {}
 
-  for (let str of strs) {
-    let sortedStr = str.split("").sort().join("");
-    // ["aet","aet","ant","aet","ant","abt"]
-
-    obj[sortedStr] ? obj[sortedStr].push(str) : (obj[sortedStr] = [str]);
-    // {"aet": ["eat", "tea", "ate"], "ant": ["tan", "nat"], "abt": ["bat"]}
-  }
-
-  return Object.values(obj);
+    for(let str of strs){
+        let s = str.split('').sort().join('')
+        if(!map[s]) map[s] = []
+        map[s].push(str)
+    }
+    return Object.values(map)
 };
