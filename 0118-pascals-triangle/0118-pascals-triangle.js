72a23efd5bf835165aac6720d53607af940d16c1
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-08-24 15:24:27 +0530

    Time: 45 ms (95.92%), Space: 42.5 MB (32.75%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  let result = [];
+  for (let i = 0; i < numRows; i++) {
+    result[i] = new Array(i + 1);
+    // result[2] = [undefined, undefined, undefined]
+
+    result[i][0] = 1;
+    result[i][i] = 1;
+    // [1, undefined, 1]
+
+    // need to manipulate value from the 1st element till the penultimate element in a column, skip the 0th and last columns as they should be 1s
+    for (let j = 1; j < i; j++) {
+      // for i = 2, add the element at previous 1st row-0th column with previous 1st row-1st column, result[1][0] + result[1][1] = 1 + 1 = 2, see Pascal's triangle image 
+      result[i][j] = result[i - 1][j - 1] + result[i - 1][j];   
+      // [1, 2, 1]
+    }
+  }
+  return result; 
+};
