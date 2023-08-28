6a6eea251aeb5809b833b171d423814f71744c3b
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-08-28 13:33:05 +0530

    Time: 54 ms (74.33%), Space: 42.4 MB (48.41%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -8,13 +8,13 @@ var longestCommonPrefix = function (strs) {
 
   // loop through the characters of first element
   for (let i = 0; i < strs[0].length; i++) {
-    let CharacterOfFirstEl = strs[0][i];
+    let currCharOfFirstEl = strs[0][i];
 
     // loop through the elements of array and compare each element's character with the first element's character at the same position
     for (let j = 0; j < strs.length; j++) {
-      if (strs[j][i] !== CharacterOfFirstEl) return commonPrefix;
+      if (strs[j][i] !== currCharOfFirstEl) return commonPrefix;
     }
-    commonPrefix += CharacterOfFirstEl;
+    commonPrefix += currCharOfFirstEl;
   }
   return commonPrefix;
 };
