69dee9f45c6cb0dfc088721dc713c5de99455d93
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-08-30 15:21:28 +0530

    Time: 59 ms (30.34%), Space: 41.9 MB (50.13%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -4,14 +4,14 @@
  */
 var lengthOfLastWord = function (s) {
   let lastWordLength = 0;
-  let rightOfFirstNonEmptyChar = true;
+  let rightOfLastNonEmptyChar = true;
 
   for (let i = s.length - 1; i >= 0; i--) {
     if (s[i] !== " ") {
       lastWordLength++;
-      rightOfFirstNonEmptyChar = false;
+      rightOfLastNonEmptyChar = false;
     } else {
-      if (!rightOfFirstNonEmptyChar) {
+      if (!rightOfLastNonEmptyChar) {
         break;
       }
     }
