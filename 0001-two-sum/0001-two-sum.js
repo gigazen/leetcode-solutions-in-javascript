d57607c7798aa5040861d392d4e16ee92969f2d0
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-09-03 13:37:36 +0530

    Time: 56 ms (84.74%), Space: 42.7 MB (42.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -9,7 +9,7 @@ var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     let complement = target - nums[i];
     if (map.has(complement)) {
-      return [i, map.get(complement)];
+      return [map.get(complement), i];
     }
     map.set(nums[i], i);
   }
