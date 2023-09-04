62b1928be1f183db92560f2e70a9f1948bc61ee0
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-09-04 16:53:05 +0530

    Time: 70 ms (73.42%), Space: 43.7 MB (69.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -1,13 +1,11 @@
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
-let isAnagram = function (s, t) {
+var isAnagram = function (s, t) {
+  // if any of the strings is not defined or falsy then not anagram, if their lengths are not same then also not anagram
+  if (!s || !t) return false;
   if (s.length !== t.length) return false;
 
-  let map = new Map();
+  const map = new Map();
 
+  // iterate through the first string and create character-count, key-value entries in map
   for (let char of s) {
     if (!map.has(char)) {
       map.set(char, 1);
@@ -16,15 +14,15 @@ let isAnagram = function (s, t) {
     }
   }
 
+  // iterate through the second string and delete character-count, key-value entries from map, if matched
   for (let char of t) {
-    if (!map.has(char)) {
-      return false;
-    } else if (map.get(char) === 1) {
+    if (!map.has(char)) return false;
+    if (map.get(char) === 1) {
       map.delete(char);
     } else {
       map.set(char, map.get(char) - 1);
     }
   }
 
-  if (map.size === 0) return true;
+  return map.size === 0;
 };
