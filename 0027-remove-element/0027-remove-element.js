8f50839fda99cce7edeb0463012f2bda61089a40
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-09-09 17:58:54 +0530

    Time: 59 ms (30.19%), Space: 42.1 MB (39.89%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -3,11 +3,13 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function (nums, val) {
-  for (let i = nums.length - 1; i >= 0; i--) {
-    if (nums[i] === val) {
-      nums.splice(i, 1);
+var removeElement = function(nums, val) {
+  let index = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[index] = nums[i];
+      index++;
     }
   }
-  return nums.length;
+  return index;
 };
