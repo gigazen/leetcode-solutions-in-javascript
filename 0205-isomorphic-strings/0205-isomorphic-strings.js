277249a3b4436858c0ea2eb71e9d5ce059c9aebf
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-09-13 17:23:36 +0530

    Time: 50 ms (96.85%), Space: 42.3 MB (77.82%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -3,16 +3,36 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function (s, t) {
-  const mapS = new Map();
-  const mapT = new Map();
-
-  for (let i = 0; i < s.length; i++) {
-    if (!mapS.has(s[i]) && !mapT.has(t[i])) {
-      mapS.set(s[i], t[i]);
-      mapT.set(t[i], s[i]);
-    } else if (mapS.get(s[i]) !== t[i] || mapT.get(t[i] !== s[i])) return false;
+function isIsomorphic(s, t) {
+  if (s.length !== t.length) {
+    return false;
   }
 
-  return true;
-};
+  const sToTMap = {}; // Map from characters in s to characters in t
+  const tToSMap = {}; // Map from characters in t to characters in s
+
+  for (let i = 0; i < s.length; i++) {
+    const charS = s[i];
+    const charT = t[i];
+
+    if (sToTMap[charS]) {
+      // If sToTMap already has a mapping, check if it matches the current character in t
+      if (sToTMap[charS] !== charT) {
+        return false; // Mismatch, not isomorphic
+      }
+    } else {
+      sToTMap[charS] = charT; // Create a new mapping
+    }
+
+    if (tToSMap[charT]) {
+      // If tToSMap already has a mapping, check if it matches the current character in s
+      if (tToSMap[charT] !== charS) {
+        return false; // Mismatch, not isomorphic
+      }
+    } else {
+      tToSMap[charT] = charS; // Create a new mapping
+    }
+  }
+
+  return true; // All characters have valid mappings, strings are isomorphic
+}
