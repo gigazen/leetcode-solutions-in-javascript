30241ecf42e967e78efbf5b42dadf158eace2ef6
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-09-14 16:15:03 +0530

    Time: 53 ms (83.25%), Space: 44.2 MB (61.69%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -5,12 +5,15 @@
  */
 var canPlaceFlowers = function (flowerbed, n) {
   for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+    // `!== 1` valid for 0 alongwith undefined also ie it includes cases when flowerbed starts or ends with 0 but `=== 0` cannot include undefined
     if (
       flowerbed[i] === 0 &&
       flowerbed[i - 1] !== 1 &&
       flowerbed[i + 1] !== 1
     ) {
+      // plant a flower there
       n--;
+      // skip the already checked (i + 1)th plot from being i as adjacent plots cannot be planted
       i++;
     }
   }
