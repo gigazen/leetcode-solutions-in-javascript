798057c8301b7e99729e8a73ac6e0c88b7aa0e25
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-09-15 11:39:23 +0530

    Time: 47 ms (97.03%), Space: 45.4 MB (31.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -2,18 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function (nums) {
-  let map = new Map();
-  let minNumberOfTimes = nums.length / 2;
+var majorityElement = function(nums) {
+    let count = 0;
+    let candidate = null;
 
-  for (let i = 0; i < nums.length; i++) {
-    let currElement = nums[i];
-    if (!map.has(currElement)) {
-      map.set(currElement, 1);
-    } else {
-      map.set(currElement, map.get(currElement) + 1);
+    for(let num of nums) {
+        if(count == 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
     }
 
-    if (map.get(currElement) > minNumberOfTimes) return currElement;
-  }
+    return candidate;
 };
