4692b564bd1404aa0ae74f3286ea607c83399825
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-09-22 18:03:57 +0530

    Time: 51 ms (93.09%), Space: 43 MB (80.06%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
@@ -2,28 +2,14 @@
  * @param {string} text
  * @return {number}
  */
-var maxNumberOfBalloons = function (text) {
-  let map = new Map([
-    ["b", 0],
-    ["a", 0],
-    ["l", 0],
-    ["o", 0],
-    ["n", 0],
-  ]);
-
-  for (let char of text) {
-    if (map.get(char) !== undefined) {
-      map.set(char, map.get(char) + 1);
+var maxNumberOfBalloons = function(text) {
+    const map = { b: 0, a: 0, l: 0, o: 0, n: 0, };
+    
+    for (const l of text) {
+        map[l]++; 
     }
-  }
-
-  return Math.floor(
-    Math.min(
-      map.get("b"),
-      map.get("a"),
-      map.get("l") / 2,
-      map.get("o") / 2,
-      map.get("n")
-    )
-  );
+    
+    return Math.floor(
+        Math.min(map.b, map.a, map.l / 2, map.o / 2, map.n)
+    );
 };
