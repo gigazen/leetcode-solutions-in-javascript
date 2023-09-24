61e2949a905420a2757b6411ac3f03d41ecfec6c
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-09-24 18:28:14 +0530

    Time: 52 ms (93.50%), Space: 41.9 MB (93.22%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,18 +4,19 @@
  * @return {boolean}
  */
 var isIsomorphic = function (s, t) {
-  const sToTMap = new Map();
-  const tToSMap = new Map();
+  let sToTMap = new Map();
+  let tToSMap = new Map();
 
   for (let i = 0; i < s.length; i++) {
-    const charS = s[i];
-    const charT = t[i];
+    let charS = s[i];
+    let charT = t[i];
 
     if (!sToTMap.has(charS) && !tToSMap.has(charT)) {
       sToTMap.set(charS, charT);
       tToSMap.set(charT, charS);
-    } else if (sToTMap.get(charS) !== charT || tToSMap.get(charT !== charS))
+    } else if (sToTMap.get(charS) !== charT || tToSMap.get(charT) !== charS) {
       return false;
+    }
   }
 
   return true;
