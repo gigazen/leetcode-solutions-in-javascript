6eb16aa1d71acab33ab61c39c22c056b1a7af43c
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-03 16:16:07 +0530

    Time: 142 ms (57.78%), Space: 55.2 MB (23.70%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.js
+++ 0705-design-hashset/0705-design-hashset.js
@@ -1,18 +1,68 @@
+// Node class to store key-value pairs
+class Node {
+  constructor(key) {
+    this.key = key;
+    this.next = null;
+  }
+}
+
+// MyHashSet class
 class MyHashSet {
   constructor() {
-    this.set = new Set();
+    this.buckets = new Array(1000).fill(null); // Adjust the size as needed
   }
 
+  // Hash function
+  hash(key) {
+    return key % this.buckets.length;
+  }
+
+  // Add key to the HashSet
   add(key) {
-    this.set.add(key);
+    const index = this.hash(key);
+
+    if (!this.buckets[index]) {
+      this.buckets[index] = new Node(key);
+    } else {
+      let current = this.buckets[index];
+      
+      while (current.next) {
+        if (current.key === key) return; // Key already exists
+        current = current.next;
+      }
+      if (current.key !== key) current.next = new Node(key);
+    }
   }
 
-  remove(key) {
-    this.set.delete(key);
-  }
-
+  // Check if key exists in the HashSet
   contains(key) {
-    return this.set.has(key);
+    const index = this.hash(key);
+    let current = this.buckets[index];
+    while (current) {
+      if (current.key === key) return true; // Key found
+      current = current.next;
+    }
+    return false; // Key not found
+  }
+
+  // Remove key from the HashSet
+  remove(key) {
+    const index = this.hash(key);
+    let current = this.buckets[index];
+    let dummy = new Node(); // Dummy node to simplify deletion
+    dummy.next = current;
+
+    let prev = dummy;
+    while (current) {
+      if (current.key === key) {
+        prev.next = current.next;
+        break;
+      }
+      prev = current;
+      current = current.next;
+    }
+
+    this.buckets[index] = dummy.next; // Update the bucket
   }
 }
 
