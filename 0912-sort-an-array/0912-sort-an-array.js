4b66aaa5f8d0a0706f0b1d98beda1d6bca0a9e84
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-05 18:23:29 +0530

    Time: 213 ms (74.24%), Space: 68.9 MB (53.37%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    quickSort(nums, 0,nums.length-1);
+    return nums
+    
+    function quickSort(nums, left , right){
+        if(left >= right)return;
+        
+        let index = partition(nums, left , right); 
+        
+        quickSort(nums, left,index-1);
+        quickSort(nums, index, right);
+        
+    }
+    
+
+    function partition(arr ,l, r){
+        let pivot = nums[Math.floor(l + Math.random() * (r - l + 1))];
+        while(l <= r) {
+            while(arr[l] < pivot ) {
+                l++;
+            }
+            while(arr[r] > pivot){
+                r--;
+            }            
+            if(l <= r ) {
+                [arr[l], arr[r]] = [ arr[r], arr[l] ];
+                l++;
+                r--;
+            }
+        }
+        return l
+    }
+};
