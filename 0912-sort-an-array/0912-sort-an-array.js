abe2745eed47fb68fe229d4fed56f849650facd8
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-06 15:55:29 +0530

    Time: 492 ms (43.66%), Space: 65.7 MB (67.01%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -3,32 +3,35 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-	let len = nums.length;
-	if(len < 2) return nums;
 
-	quickSort(nums, 0, len-1)
-	return nums
-};
+const quickSortHelper = (start, end) => {
+  
+    if(start >= end) return
+    let left = start 
+    let right = end
+    const pivot = nums[Math.floor((start+end) / 2)]
+    
+    while(left <= right) {
+        while(left <= right && pivot > nums[left]){
+            left++
+        }
+        while(left <= right && pivot < nums[right]){
+            right--
+        }
+        if(left <= right){
+            let temp = nums[left]
+            nums[left] = nums[right]
+            nums[right] = temp
+            left++
+            right--
+        }
+    }
+    quickSortHelper(start, right)
+    quickSortHelper(left, end)
+ }
+ 
+    if(nums.length === 1) return nums;
 
-var quickSort = function(nums, start, end){
-	if(start >= end) return
-	let left = start, right = end;
-	let pivot = nums[Math.floor((start+end) / 2)];
-	while(left <= right) {
-		while(left <= right && nums[left] < pivot){
-			left++
-		}
-		while(left <= right && nums[right] > pivot){
-			right--
-		}
-		if(left <= right){
-			let temp = nums[left]
-			nums[left] = nums[right]
-			nums[right] = temp
-			left++
-			right--
-		}
-	}
-	quickSort(nums, start, right)
-	quickSort(nums, left, end)
+    quickSortHelper(0, nums.length-1)
+    return nums
 }
