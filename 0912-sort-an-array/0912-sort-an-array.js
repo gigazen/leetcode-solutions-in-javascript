746ca24afae9ade8f9fca5925874fcf74950c92a
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-06 15:52:33 +0530

    Time: 441 ms (44.18%), Space: 66.3 MB (63.07%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -3,35 +3,26 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    quickSort(nums, 0,nums.length-1);
+    if (nums.length < 2) return nums
+    quickSort(nums, 0, nums.length-1)
     return nums
-    
-    function quickSort(nums, left , right){
-        if(left >= right)return;
-        
-        let index = partition(nums, left , right); 
-        
-        quickSort(nums, left,index-1);
-        quickSort(nums, index, right);
-        
-    }
-    
+}
 
-    function partition(arr ,l, r){
-        let pivot = nums[Math.floor(l + Math.random() * (r - l + 1))];
-        while(l <= r) {
-            while(arr[l] < pivot ) {
-                l++;
-            }
-            while(arr[r] > pivot){
-                r--;
-            }            
-            if(l <= r ) {
-                [arr[l], arr[r]] = [ arr[r], arr[l] ];
-                l++;
-                r--;
-            }
+var quickSort = (nums, start, end) => {
+    if (start >= end) return
+    let left = start, right=end
+    let pivot = nums[Math.floor((start+end)/2)]
+    while (left <= right) {
+        while (nums[left] < pivot) left++
+        while (nums[right] > pivot) right--
+        if (left <= right) {
+            let temp = nums[left]
+            nums[left] = nums[right]
+            nums[right] = temp
+            left++
+            right--
         }
-        return l
     }
-};
+    quickSort(nums,start,right)
+    quickSort(nums,left,end)
+}
