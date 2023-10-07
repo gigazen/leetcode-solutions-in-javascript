b328f6cde6bb8b3ad552f16eca27432e8a33b8ae
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-07 18:27:46 +0530

    Time: 418 ms (44.41%), Space: 66.1 MB (64.07%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -2,43 +2,27 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function (nums) {
-  quickSort(nums, 0, nums.length - 1);
-  return nums;
+var sortArray = function(nums) {
+    if (nums.length < 2) return nums
+    quickSort(nums, 0, nums.length-1)
+    return nums
+}
 
-  function quickSort(nums, left, right) {
-    if (left < right) {
-      let pivotIndex = partition(nums, left, right);
-
-      quickSort(nums, left, pivotIndex - 1);
-      quickSort(nums, pivotIndex + 1, right);
+var quickSort = (nums, start, end) => {
+    if (start >= end) return
+    let left = start, right=end
+    let pivot = nums[Math.floor((start+end)/2)]
+    while (left <= right) {
+        while (nums[left] < pivot) left++
+        while (nums[right] > pivot) right--
+        if (left <= right) {
+            let temp = nums[left]
+            nums[left] = nums[right]
+            nums[right] = temp
+            left++
+            right--
+        }
     }
-  }
-
-  function partition(arr, left, right) {
-    // randomly select an index and swap element at that index and at right index, make right index element as pivot element
-    let randomIndex = Math.floor(left + Math.random() * (right - left + 1));
-    [nums[randomIndex], nums[right]] = [nums[right], nums[randomIndex]];
-
-    let pivot = right;
-    let j = left;
-    let i = left - 1;
-
-    // two-pointer approach
-    while (j <= pivot) {
-      if (nums[j] < nums[pivot]) {
-        i++;
-        [nums[i], nums[j]] = [nums[j], nums[i]];
-        j++;
-      } else {
-        j++;
-      }
-    }
-
-    i++;
-    [nums[i], nums[pivot]] = [nums[pivot], nums[i]];
-
-    return i;
-  }
-};
-
+    quickSort(nums,start,right)
+    quickSort(nums,left,end)
+}
