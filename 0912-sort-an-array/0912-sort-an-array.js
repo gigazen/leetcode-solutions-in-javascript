8ecedcde44b38662cca55edd2949d2ef6f143d86
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-07 18:22:45 +0530

    Time: 476 ms (43.53%), Space: 65.8 MB (67.21%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -2,33 +2,45 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function(nums) {
-	let len = nums.length;
-	if(len < 2) return nums;
+const sortArray = (nums) => {
+  // Quick sort function
+  const quickSort = (nums, start, end) => {
+    // If start and end index cross, array is sorted
+    if (start >= end) {
+      return;
+    }
 
-	quickSort(nums, 0, len-1)
-	return nums
-};
+    // Set left and right pointers
+    let left = start, right = end;
 
-var quickSort = function(nums, start, end){
-	if(start >= end) return
-	let left = start, right = end;
-	let pivot = nums[Math.floor((start+end) / 2)];
-	while(left <= right) {
-		while(left <= right && nums[left] < pivot){
-			left++
-		}
-		while(left <= right && nums[right] > pivot){
-			right--
-		}
-		if(left <= right){
-			let temp = nums[left]
-			nums[left] = nums[right]
-			nums[right] = temp
-			left++
-			right--
-		}
-	}
-	quickSort(nums, start, right)
-	quickSort(nums, left, end)
+    // Choose pivot element
+    let pivot = nums[Math.floor((start + end) / 2)];
+
+    // Partition array
+    while (left <= right) {
+      while (left <= right && nums[left] < pivot) {
+        left++;
+      }
+      while (left <= right && nums[right] > pivot) {
+        right--;
+      }
+      if (left <= right) {
+        let temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        left++;
+        right--;
+      }
+    }
+
+    // Recursively sort left and right partitions
+    quickSort(nums, start, right);
+    quickSort(nums, left, end);
+  }
+
+  // Call quick sort function with initial start and end indices
+  quickSort(nums, 0, nums.length - 1);
+  
+  // Return sorted array
+  return nums;
 }
