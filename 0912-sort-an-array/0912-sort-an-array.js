aeaa94baa4f884bf28170cb93a3e3f3c83ab9f51
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-10 09:26:57 +0530

    Time: 208 ms (78.32%), Space: 87.7 MB (32.65%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -2,40 +2,37 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function(nums) {
-    if(nums.length <= 1) return nums
-    
-    let midIdx = Math.floor(nums.length / 2)
-    let leftArr = sortArray(nums.slice(0, midIdx))
-    let rightArr = sortArray(nums.slice(midIdx))
-    
-    return mergeArr(leftArr, rightArr)
-};
-
-var mergeArr = function(arr1, arr2) {
-    let results = []
-    let i = 0
-    let j = 0
-    
-    while(i <  arr1.length && j < arr2.length){
-        if(arr2[j] > arr1[i]){
-            results.push(arr1[i]);
+var merge = (arr1,arr2) => {
+    let res =[];
+    let i=0, j=0;
+    while(i<arr1.length && j<arr2.length){
+        if(arr1[i] <= arr2[j]){
+            res.push(arr1[i]);
             i++;
-        } else {
-            results.push(arr2[j])
+        }else{
+            res.push(arr2[j]);
             j++;
         }
     }
-    
-    while(i < arr1.length) {
-        results.push(arr1[i])
+
+    while(i<arr1.length){
+        res.push(arr1[i]);
         i++;
     }
-    
-    while(j < arr2.length) {
-        results.push(arr2[j])
+
+    while(j<arr2.length){
+        res.push(arr2[j]);
         j++;
     }
-    
-     return results;
+    return res;
+}
+
+var sortArray = function(nums) {
+    if(nums.length <= 1) return nums;
+
+    let mid = Math.floor(nums.length/2);
+    let left = sortArray(nums.slice(0,mid));
+    let right = sortArray(nums.slice(mid));
+
+    return merge(left,right);
 }
