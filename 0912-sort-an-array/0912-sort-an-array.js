431a36bde68e1c594a173acc9adee6ce5f69bf8c
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-10 09:22:51 +0530

    Time: 235 ms (58.99%), Space: 88 MB (30.35%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -2,23 +2,58 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function(nums) {
-    if(nums.length < 2) return nums
-    let mid = Math.floor(nums.length/2);
-    let left = nums.slice(0, mid);
-    let right = nums.slice(mid);
-    return merge(sortArray(left), sortArray(right));
+var sortArray = function (nums) {
+  if (nums.length === 1) {
+    return nums;
+  }
+  let mid = Math.round(nums.length / 2);
+  if (nums.length < 2) {
+    return;
+  }
+  let left = [];
+  let right = [];
+  for (let i = 0; i < mid; i++) {
+    left.push(nums[i]);
+  }
+
+  for (let j = mid; j < nums.length; j++) {
+    right.push(nums[j]);
+  }
+
+  sortArray(left);
+  sortArray(right);
+  mergeSortedArray(left, right, nums);
+
+  return nums;
 };
 
-var merge = function(left, right) {
-    let sortedArray = [];
-    while(left.length && right.length){
-        if(left[0] <= right[0]){
-            sortedArray.push(left.shift())
-        }else {
-            sortedArray.push(right.shift())
-        }
-    }
+function mergeSortedArray(left, right, nums) {
+  let i = 0;
+  let j = 0;
+  let k = 0;
 
-    return [...sortedArray, ...left, ...right]
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      nums[k] = left[i];
+      i++;
+    } else {
+      nums[k] = right[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (j < right.length) {
+    nums[k] = right[j];
+    j++;
+    k++;
+  }
+
+  while (i < left.length) {
+    nums[k] = left[i];
+    k++;
+    i++;
+  }
+
+  return nums;
 }
