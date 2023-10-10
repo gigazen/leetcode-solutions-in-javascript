cfb25fbb1142373b43f429f55c79fd7852b9e106
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-10 09:25:16 +0530

    Time: 239 ms (58.10%), Space: 87.9 MB (30.35%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -2,58 +2,40 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function (nums) {
-  if (nums.length === 1) {
-    return nums;
-  }
-  let mid = Math.round(nums.length / 2);
-  if (nums.length < 2) {
-    return;
-  }
-  let left = [];
-  let right = [];
-  for (let i = 0; i < mid; i++) {
-    left.push(nums[i]);
-  }
-
-  for (let j = mid; j < nums.length; j++) {
-    right.push(nums[j]);
-  }
-
-  sortArray(left);
-  sortArray(right);
-  mergeSortedArray(left, right, nums);
-
-  return nums;
+var sortArray = function(nums) {
+    if(nums.length <= 1) return nums
+    
+    let midIdx = Math.floor(nums.length / 2)
+    let leftArr = sortArray(nums.slice(0, midIdx))
+    let rightArr = sortArray(nums.slice(midIdx))
+    
+    return mergeArr(leftArr, rightArr)
 };
 
-function mergeSortedArray(left, right, nums) {
-  let i = 0;
-  let j = 0;
-  let k = 0;
-
-  while (i < left.length && j < right.length) {
-    if (left[i] <= right[j]) {
-      nums[k] = left[i];
-      i++;
-    } else {
-      nums[k] = right[j];
-      j++;
+var mergeArr = function(arr1, arr2) {
+    let results = []
+    let i = 0
+    let j = 0
+    
+    while(i <  arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j])
+            j++;
+        }
     }
-    k++;
-  }
-
-  while (j < right.length) {
-    nums[k] = right[j];
-    j++;
-    k++;
-  }
-
-  while (i < left.length) {
-    nums[k] = left[i];
-    k++;
-    i++;
-  }
-
-  return nums;
+    
+    while(i < arr1.length) {
+        results.push(arr1[i])
+        i++;
+    }
+    
+    while(j < arr2.length) {
+        results.push(arr2[j])
+        j++;
+    }
+    
+     return results;
 }
