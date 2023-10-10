d24802b2d08bd060c1508eae05b7acf5fddb220f
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-10 09:29:34 +0530

    Time: 197 ms (83.80%), Space: 64.6 MB (79.02%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -2,26 +2,32 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const sortArray = nums => {
-    if(nums.length <= 1) return nums
-    
-    const middle = Math.floor(nums.length / 2)
-    const left = nums.slice(0, middle)
-    const right = nums.slice(middle)
-    
-    return merge(sortArray(left), sortArray(right))
+function sortArray(nums) {
+  sort(nums);
+  return nums;
 };
 
-const merge = (left, right) => {
-    const result = []
-    
-    while (left.length && right.length) {
-        if(left[0] <= right[0]) {
-            result.push(left.shift())
-        } else {
-            result.push(right.shift())
-        }
-    }
-    
-    return [...result, ...left, ...right]
+function sort(nums) {
+  mergeSort(nums, [], 0, nums.length - 1);
+}
+
+function mergeSort(nums, aux, lo, hi) {
+  if (lo >= hi) return;
+  const mid = Math.floor((hi - lo) / 2) + lo;
+  mergeSort(nums, aux, lo, mid);
+  mergeSort(nums, aux, mid + 1, hi);
+  merge(nums, aux, lo, mid, hi);
+}
+
+function merge(nums, aux, lo, mid, hi) {
+  for (let i = lo; i <= hi; i++) {
+    aux[i] = nums[i];
+  }
+  
+  for (let i = lo, j = mid + 1, k = lo; k <= hi; k++) {
+    if (i > mid) nums[k] = aux[j++];
+    else if (j > hi) nums[k] = aux[i++];
+    else if (aux[i] < aux[j]) nums[k] = aux[i++];
+    else nums[k] = aux[j++];
+  }
 }
