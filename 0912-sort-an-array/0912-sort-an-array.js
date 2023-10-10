261853b0003bb9fc5d8d56616e1d7e902f4a48aa
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-10 09:28:37 +0530

    Time: 1172 ms (29.33%), Space: 92.1 MB (5.10%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -2,37 +2,26 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var merge = (arr1,arr2) => {
-    let res =[];
-    let i=0, j=0;
-    while(i<arr1.length && j<arr2.length){
-        if(arr1[i] <= arr2[j]){
-            res.push(arr1[i]);
-            i++;
-        }else{
-            res.push(arr2[j]);
-            j++;
+const sortArray = nums => {
+    if(nums.length <= 1) return nums
+    
+    const middle = Math.floor(nums.length / 2)
+    const left = nums.slice(0, middle)
+    const right = nums.slice(middle)
+    
+    return merge(sortArray(left), sortArray(right))
+};
+
+const merge = (left, right) => {
+    const result = []
+    
+    while (left.length && right.length) {
+        if(left[0] <= right[0]) {
+            result.push(left.shift())
+        } else {
+            result.push(right.shift())
         }
     }
-
-    while(i<arr1.length){
-        res.push(arr1[i]);
-        i++;
-    }
-
-    while(j<arr2.length){
-        res.push(arr2[j]);
-        j++;
-    }
-    return res;
-}
-
-var sortArray = function(nums) {
-    if(nums.length <= 1) return nums;
-
-    let mid = Math.floor(nums.length/2);
-    let left = sortArray(nums.slice(0,mid));
-    let right = sortArray(nums.slice(mid));
-
-    return merge(left,right);
+    
+    return [...result, ...left, ...right]
 }
