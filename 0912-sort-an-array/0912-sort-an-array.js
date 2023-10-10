ac4a9a9be6150458a2f30a5b81e3f9400b7a2e8c
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-10 09:39:10 +0530

    Time: 204 ms (80.80%), Space: 65 MB (76.85%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -3,12 +3,8 @@
  * @return {number[]}
  */
 function sortArray(nums) {
-  sort(nums);
-  return nums;
-};
-
-function sort(nums) {
   mergeSort(nums, [], 0, nums.length - 1);
+  return nums;
 }
 
 function mergeSort(nums, aux, lo, hi) {
@@ -23,7 +19,7 @@ function merge(nums, aux, lo, mid, hi) {
   for (let i = lo; i <= hi; i++) {
     aux[i] = nums[i];
   }
-  
+
   for (let i = lo, j = mid + 1, k = lo; k <= hi; k++) {
     if (i > mid) nums[k] = aux[j++];
     else if (j > hi) nums[k] = aux[i++];
