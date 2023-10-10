003f1e0c933099f2815faaf5edc178697fab3667
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-10 09:34:15 +0530

    Time: 1189 ms (29.01%), Space: 91.3 MB (6.69%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -3,39 +3,18 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    mergeSort(nums, 0 , nums.length-1)
-    return nums
+    if (nums.length < 2) return nums
+    let mid = Math.floor(nums.length/2)
+    let left = nums.slice(0, mid)
+    let right = nums.slice(mid)
+    return merge(sortArray(left), sortArray(right))
 };
 
-const mergeSort = (array, left, right) => {
-    if(left < right){
-        const mid = Math.floor((left + right) / 2);
-        mergeSort(array, left, mid);
-        mergeSort(array, mid+1, right);
-        merge(array, left, mid, right);
+var merge = (arr1, arr2) => {
+    let arr = []
+    while (arr1.length && arr2.length) {
+        if (arr1[0] <= arr2[0]) arr.push(arr1.shift())
+        else arr.push(arr2.shift())
     }
-};
-
-const merge = (array, left, mid, right) => {
-    const leftArr = [], rightArr = [];
-    const M = mid - left + 1 , N = right - mid;
-
-    for (let index = 0; index < M; index++)
-        leftArr.push(array[left + index]);
-    for (let index = 0; index < N; index++)
-        rightArr.push(array[mid + 1 + index]);
-  
-    let i = 0, j = 0, k = left;
-
-    while (i < M && j < N) {
-        if (leftArr[i] <= rightArr[j])
-            array[k++] = leftArr[i++];
-        else 
-            array[k++] = rightArr[j++];
-    }
-
-    while (i < M) array[k++] = leftArr[i++];
-  
-    while (j < N) array[k++] = rightArr[j++];
-
-};
+    return [...arr, ...arr1, ...arr2]
+}
