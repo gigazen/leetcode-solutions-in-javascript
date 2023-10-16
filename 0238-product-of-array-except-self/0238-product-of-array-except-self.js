e22fbdb33b6c83626dedad50b551932a77523229
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-16 09:39:54 +0530

    Time: 108 ms (21.21%), Space: 52.2 MB (96.46%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,17 +3,16 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let output = []
-    
-    nums.reduceRight((prev, acc, i) => {
-        output[i] = prev
-        return prev * acc
-    }, 1)
-    
-    nums.reduce((prev, acc, i) => {
-        output[i] *= prev
-        return prev * acc
-    }, 1)
-    
-    return output
+    var output = [];
+    var leftMult = 1;
+    var rightMult = 1;
+    for (var i=nums.length - 1; i >= 0; i--) {
+        output[i] = rightMult;
+        rightMult *= nums[i];
+    }
+    for (var j=0; j < nums.length; j++) {
+        output[j] *= leftMult;
+        leftMult *= nums[j];
+    }
+    return output;
 };
