f1647c2897e41ca25bfa5708c4894a7e519f353d
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-16 09:31:06 +0530

    Time: 88 ms (59.42%), Space: 55.5 MB (28.33%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,43 +3,21 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    // First, create a prefix array that moves from the left,
-    // gathering the running product of the prefix at each index
-    const prefix = []
-    
-    // Move left in the input array
-    for (let i=0; i<nums.length; i++) {
-        // If i === 0, start with `1`, since there is no prefix
-        if (i===0) {
-            prefix[i] = 1
-        } else {
-            // Otherwise, multiply nums[i-1] times the prefix at position i-1,
-            // and add that to the prefix array at position i
-            prefix[i] = nums[i-1] * prefix[i-1]
-        }
-    }
-    
-    // Then, let's create a suffix array
-    const suffix = []
-    
-    // Move right in the input array
-    for (let i=nums.length - 1; i>=0; i--) {
-        // For the last index, we have no suffix, so just add a 1 to that position
-        if (i===nums.length - 1) {
-            suffix[i] = 1
-        } else {
-            // Otherwise, we multiply nums[i+1] by the suffix at position i+1
-            // and add that to the suffix array at position i
-            suffix[i] = nums[i + 1] * suffix[i + 1]
-        }
-    }
-    
-    // Finally, our result array should be the products of prefix * suffix for each position
-    const result = []
-    
-    for (let i=0; i<nums.length; i++) {
-        result[i] = prefix[i] * suffix[i]
-    }
-    
-    return result
+  let result = [];
+  let left = 1;
+  let right = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    // compute left product for nums[i]
+    result[i] = left;
+    left *= nums[i];
+  }
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    // compute right product for nums[i] and multiply it with the left product
+    result[i] *= right;
+    right *= nums[i];
+  }
+
+  return result;
 };
