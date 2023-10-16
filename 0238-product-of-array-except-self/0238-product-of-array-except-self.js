9520635526ed0d644d799c5311cdce302710d346
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-16 09:39:14 +0530

    Time: 84 ms (73.21%), Space: 52.3 MB (94.74%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,18 +3,17 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let result = Array(nums.length).fill(1)
-
-    let prodOfNumsBehindLeftPointer = 1
-    let prodOfNumsBehindRigthtPointer = 1
+    let output = []
     
-    for(let l = 0, r = nums.length - 1; l < nums.length; l++, r--){
-        result[l] *= prodOfNumsBehindLeftPointer
-        result[r] *= prodOfNumsBehindRigthtPointer
-
-        prodOfNumsBehindLeftPointer *= nums[l]
-        prodOfNumsBehindRigthtPointer *= nums[r]
-    }
-
-    return result
+    nums.reduceRight((prev, acc, i) => {
+        output[i] = prev
+        return prev * acc
+    }, 1)
+    
+    nums.reduce((prev, acc, i) => {
+        output[i] *= prev
+        return prev * acc
+    }, 1)
+    
+    return output
 };
