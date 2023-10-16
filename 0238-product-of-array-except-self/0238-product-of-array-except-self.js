ef097f906f2c2f24bdff1f685df58d160230729f
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-16 09:29:47 +0530

    Time: 90 ms (52.12%), Space: 53.7 MB (72.05%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,20 +3,43 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const resultArr = []
-    for(let i=0; i<nums.length; i++){
-        let productVal = nums[i]
-        if(resultArr.length !== 0)
-            productVal = productVal * resultArr[i-1]
-        resultArr.push(productVal)
+    // First, create a prefix array that moves from the left,
+    // gathering the running product of the prefix at each index
+    const prefix = []
+    
+    // Move left in the input array
+    for (let i=0; i<nums.length; i++) {
+        // If i === 0, start with `1`, since there is no prefix
+        if (i===0) {
+            prefix[i] = 1
+        } else {
+            // Otherwise, multiply nums[i-1] times the prefix at position i-1,
+            // and add that to the prefix array at position i
+            prefix[i] = nums[i-1] * prefix[i-1]
+        }
     }
     
-    let productVal = 1
-    let i=nums.length-1
-    for(i; i > 0; i--){
-        resultArr[i] = resultArr[i-1] * productVal
-        productVal = nums[i] * productVal
+    // Then, let's create a suffix array
+    const suffix = []
+    
+    // Move right in the input array
+    for (let i=nums.length - 1; i>=0; i--) {
+        // For the last index, we have no suffix, so just add a 1 to that position
+        if (i===nums.length - 1) {
+            suffix[i] = 1
+        } else {
+            // Otherwise, we multiply nums[i+1] by the suffix at position i+1
+            // and add that to the suffix array at position i
+            suffix[i] = nums[i + 1] * suffix[i + 1]
+        }
     }
-    resultArr[i] = productVal
-    return resultArr
+    
+    // Finally, our result array should be the products of prefix * suffix for each position
+    const result = []
+    
+    for (let i=0; i<nums.length; i++) {
+        result[i] = prefix[i] * suffix[i]
+    }
+    
+    return result
 };
