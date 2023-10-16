0f6b0a3a4a10618c30ab90cbb40fbe7eaee75d96
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-16 12:33:25 +0530

    Time: 81 ms (82.51%), Space: 54.8 MB (45.03%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,25 +2,24 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function(nums) {
-    if (nums === null || nums.length <= 1) {
-        return [];
-    }
-    let n = nums.length;
-    let left = new Array(n), right = new Array(n), res = new Array(n);
-    for (let i = 0; i < n; i++) {
-        left[i] = i > 0 ? nums[i] * left[i-1] : nums[i];
-    }
-    for (let i = n-1; i >= 0; i--) {
-        right[i] = i < n-1 ? nums[i] * right[i+1] : nums[i];
-    }
-    for (let i = 1; i < n-1; i++) {
-        res[i] = left[i-1] * right[i+1];
-    }
-    res[0] = right[1];
-    res[n-1] = left[n-2];
-    return res;
-    // T.C: O(N)
-    // S.C: O(N), even though we assume that the output array is not counted as extra space,
-    // we use two extra arrays of length n
+var productExceptSelf = function (nums) {
+  if (nums === null || nums.length <= 1) {
+    return [];
+  }
+    
+  let runningProdFromLeft = 1,
+    runningProdFromRight = 1;
+  let result = new Array(nums.length).fill(1);
+
+  for (let i = 0; i < nums.length; i++) {
+    result[i] *= runningProdFromLeft;
+    runningProdFromLeft *= nums[i];
+  }
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= runningProdFromRight;
+    runningProdFromRight *= nums[i];
+  }
+
+  return result; 
 };
