65b20dbb7b623f9e2ee6e180da597c89e3427cb5
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-25 10:01:41 +0530

    Time: 45 ms (98.98%), Space: 44.1 MB (26.02%) - LeetHub

---
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.js
@@ -0,0 +1,25 @@
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+let codeDB = new Map(), urlDB = new Map()
+const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+const getCode = () => {
+    let code = new Array(6).fill()
+        .map(_ => chars.charAt(~~(Math.random() * 62)))
+    return "http://tinyurl.com/" + code.join("")
+}
+
+const encode = longUrl => {
+    if (urlDB.has(longUrl)) return urlDB.get(longUrl)
+    let code = getCode()
+    while (codeDB.has(code)) code = getCode()
+    codeDB.set(code, longUrl)
+    urlDB.set(longUrl, code)
+    return code
+}
+
+const decode = shortUrl => codeDB.get(shortUrl)
