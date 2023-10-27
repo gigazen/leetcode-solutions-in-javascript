50cb5628c2d7fef20625fb04bb9efacb3d99c5b4
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-10-27 13:22:12 +0530

    Time: 61 ms (79.35%), Space: 48.3 MB (53.26%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.js
+++ 0554-brick-wall/0554-brick-wall.js
@@ -2,16 +2,19 @@
  * @param {number[][]} wall
  * @return {number}
  */
-var leastBricks = function(wall) {
-    const counts = {};
-    let max = 0;
-    for (let row of wall) {
-        let sum = 0;
-        for (let i = 0; i < row.length - 1; i++) {
-            sum += row[i];
-            counts[sum] = (counts[sum] || 0) + 1;
-            max = Math.max(max, counts[sum]);
-        }
+var leastBricks = function (wall) {
+  let freqMap = new Map();
+  let maxFreq = 0;
+
+  for (let row of wall) {
+    let runningSumInRow = 0;
+
+    for (let i = 0; i < row.length - 1; i++) {
+      runningSumInRow += row[i];
+      freqMap.set(runningSumInRow, (freqMap.get(runningSumInRow) || 0) + 1);
+      maxFreq = Math.max(maxFreq, freqMap.get(runningSumInRow));
     }
-    return wall.length - max;
+  }
+
+  return wall.length - maxFreq;
 };
