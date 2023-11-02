d5896c19d3dbbcc4138588875256778c6a0531a5
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-11-02 11:30:59 +0530

    Time: 67 ms (88.00%), Space: 49.9 MB (87.24%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -3,15 +3,16 @@
  * @param {number} k
  * @return {number}
  */
-var subarraySum = function(nums, k) {
-    let map = new Map();
-    let sum = 0;
-    let count = 0;
-    map.set(0,1);
-    for (let i=0;i<nums.length;i++) {
-        sum += nums[i];
-        if (map.has(sum-k)) count += map.get(sum-k);
-        map.set(sum, (map.get(sum) || 0) + 1);
-    }
-    return count;
-}
+var subarraySum = function (nums, k) {
+  let [curSum, numOfSubarr] = [0, 0];
+  let map = new Map();
+  map.set(0, 1);
+
+  for (let i = 0; i < nums.length; i++) {
+    curSum += nums[i];
+
+    numOfSubarr += map.get(curSum - k) || 0;
+    map.set(curSum, 1 + (map.get(curSum) || 0));
+  }
+  return numOfSubarr;
+};
