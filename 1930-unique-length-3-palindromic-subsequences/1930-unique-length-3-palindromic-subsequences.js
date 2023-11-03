5095f060d39a7564627752aa6978f618e5e6d22a
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-11-03 13:03:53 +0530

    Time: 520 ms (60.24%), Space: 49.8 MB (65.06%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.js
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.js
@@ -3,47 +3,17 @@
  * @return {number}
  */
 var countPalindromicSubsequence = function (s) {
-  const isPalindrome = (s) => {
-    let halfN = Math.trunc(s.length / 2);
-    let n = s.length;
-    for (let i = 0; i < halfN; i++) {
-      if (s[i] !== s[n - 1 - i]) {
-        return false;
-      }
-    }
-    return true;
-  };
+  let uniqCharsInS = new Set(s);
+  let countPS = 0;
 
-  var isSubsequence = function (s, t) {
-    // Edge cases
-    if (s === t) return true;
-    if (s.length === 0) return true;
-    if (s.length > t.length) return false;
-
-    let indexS = 0;
-    for (let i = 0; i < t.length; i++) {
-      if (s[indexS] === t[i]) {
-        indexS++;
-      }
-    }
-
-    return indexS === s.length;
-  };
-
-  let small = "abcdefghijklmnopqrstuvwxyz";
-  let count = 0;
-
-  for (let ch1 of small) {
-    for (let ch2 of small) {
-      for (let ch3 of small) {
-        const strOf3Chars = ch1 + ch2 + ch3;
-        if (isPalindrome(strOf3Chars)) {
-          if (isSubsequence(strOf3Chars, s)) {
-            count++;
-          }
-        }
-      }
-    }
+  for (let char of uniqCharsInS) {
+    let firstOccOfChar = s.indexOf(char);
+    let lastOccOfChar = s.lastIndexOf(char);
+    let noOfUniqCharsInBtwn = new Set(
+      s.slice(firstOccOfChar + 1, lastOccOfChar)
+    ).size;
+    countPS += noOfUniqCharsInBtwn;
   }
-  return count;
+
+  return countPS;
 };
