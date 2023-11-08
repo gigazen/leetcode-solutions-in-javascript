95ce9785b4a3bce8a1e25b94e9f4e95bc319230c
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-11-08 12:59:50 +0530

    Time: 91 ms (47.22%), Space: 57.8 MB (30.56%) - LeetHub

--- 2017-grid-game/2017-grid-game.js
+++ 2017-grid-game/2017-grid-game.js
@@ -3,27 +3,21 @@
  * @return {number}
  */
 var gridGame = function(grid) {
-    const n = grid[0].length;
-    
-    let topTotSum = grid[0].reduce((acc, num) => acc + num, 0);
-    let botTotSum = grid[1].reduce((acc, num) => acc + num, 0);
-    
-    let leftAccBotSum = 0;
-    
-    let bestSum = Math.min(topTotSum, botTotSum);
-    
-    for (let j = 0; j < n; ++j) {
-        const topNum = grid[0][j];
-        const botNum = grid[1][j];
-        
-        topTotSum -= topNum;
-        
-        const maxSum = Math.max(topTotSum, leftAccBotSum);
-        
-        bestSum = Math.min(bestSum, maxSum);
-        
-        leftAccBotSum += botNum;
+    let len = grid[0].length;
+    let prefixTop = grid[0].slice();
+    let prefixBottom = grid[1].slice();
+    for (let i = 1; i < len; i++) {
+        prefixTop[i] += prefixTop[i-1];
+        prefixBottom[i] += prefixBottom[i-1];
     }
-    
-    return bestSum;
-};
+    let result = Infinity;
+    for (let j = 0; j < len; j++) {
+        let top = prefixTop[len - 1] - prefixTop[j];
+        let bottom = j > 0 ? prefixBottom[j - 1] : 0;
+		// From the current index, find the max points that Robot 2 can attain from either the top or bottom.
+        let r2 = Math.max(top, bottom);
+		// Keep track of the lowest max points value of all the indices visited.
+        result = Math.min(result, r2);
+    }
+    return result;
+}
