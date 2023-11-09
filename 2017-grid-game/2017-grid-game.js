7f6605d8d2a1bdfa69ff184f290da446b0f46125
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-11-09 10:31:38 +0530

    Time: 92 ms (47.22%), Space: 53.2 MB (80.56%) - LeetHub

--- 2017-grid-game/2017-grid-game.js
+++ 2017-grid-game/2017-grid-game.js
@@ -2,25 +2,19 @@
  * @param {number[][]} grid
  * @return {number}
  */
-// 88%, 30%
 var gridGame = function (grid) {
-  let noOfCols = grid[0].length;
-  let topRowPfxSum = grid[0].slice();
-  let botRowPfxSum = grid[1].slice();
-
-  for (let i = 1; i < noOfCols; i++) {
-    topRowPfxSum[i] += topRowPfxSum[i - 1];
-    botRowPfxSum[i] += botRowPfxSum[i - 1];
-  }
-
+  let topRightPfxSum = grid[0].reduce((acc, num) => acc + num, 0);
+  let botLeftPfxSum = 0;
   let lowestMaxPtOfRobot2 = Infinity;
-  for (let i = 0; i < noOfCols; i++) {
-    let topRightPfxSum = topRowPfxSum[noOfCols - 1] - topRowPfxSum[i];
-    let botLeftPfxSum = i > 0 ? botRowPfxSum[i - 1] : 0;
-    // From the current index, find the max points that Robot2 can collect from either the topRightPfxSum or botLeftPfxSum
+
+  for (let i = 0; i < grid[0].length; i++) {
+    topRightPfxSum -= grid[0][i];
+    // From the current i index, find the max points that Robot2 can collect from either the topRightPfxSum or botLeftPfxSum
     let maxPointOfRobot2 = Math.max(topRightPfxSum, botLeftPfxSum);
     // Keep track of the lowest max points value of all the indices visited because Robot1 will minimize Robot2's max points
     lowestMaxPtOfRobot2 = Math.min(lowestMaxPtOfRobot2, maxPointOfRobot2);
+    botLeftPfxSum += grid[1][i];
   }
+
   return lowestMaxPtOfRobot2;
 };
