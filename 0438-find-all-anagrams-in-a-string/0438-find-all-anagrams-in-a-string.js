6f68fc39fa3224c62ff6262c25482c6c925a101c
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-11-11 12:50:10 +0530

    Time: 87 ms (67.25%), Space: 45.6 MB (74.54%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -3,39 +3,27 @@
  * @param {string} p
  * @return {number[]}
  */
-const findAnagrams = function (s, p) {
-  const map = {}
-  for (const c of p) {
-    map[c] = (map[c] || 0) + 1
-  }
-  const currentMap = {}
-  let count = 0
-  const result = []
-  for (let i = 0; i < p.length; i++) {
-    const c = s[i]
-    currentMap[c] = (currentMap[c] || 0) + 1
-    if (currentMap[c] <= map[c]) {
-      count += 1
+var findAnagrams = function(s, p) { 
+    let hash = {},  uniqueChars = 0;
+    for (let c of p) {
+        if (hash[c]==null) {
+            uniqueChars++;
+            hash[c] = 1;
+        } else {
+            hash[c]++;
+        }
     }
-  }
-  if (count === p.length) {
-    result.push(0)
-  }
-
-  for (let i = p.length; i < s.length; i++) {
-    const c = s[i]
-    const prevC = s[i - p.length]
-    currentMap[c] = (currentMap[c] || 0) + 1
-    if (currentMap[c] <= map[c]) {
-      count += 1
+    
+    let res = [];
+    let left = 0, right = 0;
+    for (right;right<s.length;right++) {
+        if (hash[s[right]]!=null) hash[s[right]]--;
+        if (hash[s[right]]==0) uniqueChars--;
+        if (uniqueChars==0) res.push(left);
+        if (right - left + 1 == p.length) {
+            if (hash[s[left]]!=null) hash[s[left]]++;
+            if (hash[s[left++]]==1) uniqueChars++;
+        }
     }
-    currentMap[prevC] = (currentMap[prevC] || 0) - 1
-    if (currentMap[prevC] < map[prevC]) {
-      count -= 1
-    }
-    if (count === p.length) {
-      result.push(i - p.length + 1)
-    }
-  }
-  return result
-}
+    return res;
+};
