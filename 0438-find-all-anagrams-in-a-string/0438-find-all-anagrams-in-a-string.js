5f5778e12572e5e1a9cd4e96688bf2ad158a6d31
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-11-12 19:52:34 +0530

    Time: 96 ms (51.86%), Space: 46.9 MB (54.60%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -3,44 +3,32 @@
  * @param {string} p
  * @return {number[]}
  */
-function findAnagrams(s, p) {
-  let result = [];
-  let pMap = {};
-  let sMap = {};
+var findAnagrams = function (s, p) {
+  const arr = [];
+  const obj = {};
 
-  for (let char of p) {
-    pMap[char] = (pMap[char] || 0) + 1;
+  for (let i of p) {
+    obj[i] ? (obj[i] += 1) : (obj[i] = 1);
   }
 
   let left = 0;
   let right = 0;
-  let count = 0;
+  let count = p.length;
 
   while (right < s.length) {
-    const char = s[right];
+    if (obj[s[right]] > 0) count--;
 
-    if (pMap[char] !== undefined) {
-      sMap[char] = (sMap[char] || 0) + 1;
-      count++;
-
-      while (sMap[char] > pMap[char]) {
-        const leftChar = s[left];
-        sMap[leftChar]--;
-        count--;
-        left++;
-      }
-
-      if (count === p.length) {
-        result.push(left);
-      }
-    } else {
-      sMap = {};
-      count = 0;
-      left = right + 1;
-    }
-
+    obj[s[right]]--;
     right++;
-  }
 
-  return result;
-}
+    if (count === 0) arr.push(left);
+
+    if (right - left == p.length) {
+      if (obj[s[left]] >= 0) count++;
+
+      obj[s[left]]++;
+      left++;
+    }
+  }
+  return arr;
+};
