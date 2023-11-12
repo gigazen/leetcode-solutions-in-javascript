f581c04f2ec230b5cc8362f526d7b081f8af0096
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-11-12 16:21:53 +0530

    Time: 93 ms (55.25%), Space: 45.7 MB (74.84%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -3,44 +3,35 @@
  * @param {string} p
  * @return {number[]}
  */
-function findAnagrams(s, p) {
-  let result = [];
-  let pMap = {};
-  let sMap = {};
-
-  for (let char of p) {
-    pMap[char] = (pMap[char] || 0) + 1;
-  }
-
-  let left = 0;
-  let right = 0;
-  let count = 0;
-
-  while (right < s.length) {
-    const char = s[right];
-
-    if (pMap[char] !== undefined) {
-      sMap[char] = (sMap[char] || 0) + 1;
-      count++;
-
-      while (sMap[char] > pMap[char]) {
-        const leftChar = s[left];
-        sMap[leftChar]--;
-        count--;
-        left++;
-      }
-
-      if (count === p.length) {
-        result.push(left);
-      }
-    } else {
-      sMap = {};
-      count = 0;
-      left = right + 1;
+var findAnagrams = function(s, p) {
+    if(s.length < p.length)return [];
+    
+    let ans = []
+    let len = p.length;
+    let sArr = new Array(26).fill(0);
+    let pArr = new Array(26).fill(0);
+    for(let i=0; i<len; i++){
+        let charCode = p[i].charCodeAt(i);
+        sArr[s[i].charCodeAt(0) -97]++;
+        pArr[p[i].charCodeAt(0) -97]++;
     }
-
-    right++;
-  }
-
-  return result;
-}
+    if(isAnaGram(sArr, pArr))ans.push(0);
+    
+    
+    for(let i=1; i<= s.length-p.length; i++) {
+        
+        sArr[s[i-1].charCodeAt(0)-97]--;
+        sArr[s[i+len-1].charCodeAt(0) -97]++;
+        
+        if(isAnaGram(sArr, pArr))ans.push(i)
+        
+    }
+    return ans;
+    
+    function isAnaGram(arr1, arr2){
+        for(let i=0; i<=25; i++){
+            if(arr1[i] !== arr2[i])return false
+        }
+        return true
+    }
+};
