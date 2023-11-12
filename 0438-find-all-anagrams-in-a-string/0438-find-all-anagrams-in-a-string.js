7d5b96b25413f417d9f6052347f561b6be2149a1
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-11-12 17:32:53 +0530

    Time: 225 ms (20.02%), Space: 50.5 MB (13.79%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -3,35 +3,40 @@
  * @param {string} p
  * @return {number[]}
  */
-var findAnagrams = function(s, p) {
-    if(s.length < p.length)return [];
-    
-    let ans = []
-    let len = p.length;
-    let sArr = new Array(26).fill(0);
-    let pArr = new Array(26).fill(0);
-    for(let i=0; i<len; i++){
-        let charCode = p[i].charCodeAt(i);
-        sArr[s[i].charCodeAt(0) -97]++;
-        pArr[p[i].charCodeAt(0) -97]++;
+var findAnagrams = function (s, p) {
+  if (s.length < p.length) return [];
+  let pCharFreqMap = {};
+  let sCharFreqMap = {};
+  let result = [];
+
+  for (let i = 0; i < p.length; i++) {
+    pCharFreqMap[p[i]] = 1 + (pCharFreqMap[p[i]] || 0);
+    sCharFreqMap[s[i]] = 1 + (sCharFreqMap[s[i]] || 0);
+  }
+
+  result = areObjectsEqual(sCharFreqMap, pCharFreqMap) ? [0] : [];
+
+  let leftIdx = 0;
+  for (let rightIdx = p.length; rightIdx < s.length; rightIdx++) {
+    sCharFreqMap[s[rightIdx]] = 1 + (sCharFreqMap[s[rightIdx]] || 0); 
+    sCharFreqMap[s[leftIdx]]--; 
+    if (sCharFreqMap[s[leftIdx]] === 0) {
+      delete sCharFreqMap[s[leftIdx]]; 
     }
-    if(isAnaGram(sArr, pArr))ans.push(0);
-    
-    
-    for(let i=1; i<= s.length-p.length; i++) {
-        
-        sArr[s[i-1].charCodeAt(0)-97]--;
-        sArr[s[i+len-1].charCodeAt(0) -97]++;
-        
-        if(isAnaGram(sArr, pArr))ans.push(i)
-        
+    leftIdx++; 
+
+    if (areObjectsEqual(sCharFreqMap, pCharFreqMap)) {
+      result.push(leftIdx);
     }
-    return ans;
-    
-    function isAnaGram(arr1, arr2){
-        for(let i=0; i<=25; i++){
-            if(arr1[i] !== arr2[i])return false
-        }
-        return true
+  }
+
+  function areObjectsEqual(obj1, obj2) {
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+    for (const key in obj1) {
+      if (obj1[key] !== obj2[key]) return false;
     }
+    return true;
+  }
+
+  return result;
 };
