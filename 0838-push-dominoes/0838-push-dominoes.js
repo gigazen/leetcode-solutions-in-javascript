2d846e2e82d2d24a467d88bfafbd61ab8f3882f0
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-12-14 21:17:40 +0530

    Time: 103 ms (54.44%), Space: 55.7 MB (93.33%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.js
+++ 0838-push-dominoes/0838-push-dominoes.js
@@ -10,30 +10,34 @@ var pushDominoes = function (dominoes) {
   for (let i = 0, j = 1; j < n; j++) {
     if (arr[j] === ".") continue;
 
-    if (arr[i] === "L" && arr[j] === "L") {
-      // make all dots as L
-      for (let k = i + 1; k < j; k++) arr[k] = "L";
-    } else if (arr[i] === "R" && arr[j] === "R") {
-      // make all dots as R
-      for (let k = i + 1; k < j; k++) arr[k] = "R";
-    } else if (arr[i] === "L" && arr[j] === "R") {
-      // nothing to do
-    } else {
-      // solve acc. to odd and even number of dots
-      let mid = Math.floor((i + j) / 2);
-      if ((j - i) % 2 === 0) {
-        // odd dots
-        for (let k = i + 1; k < mid; k++) arr[k] = "R";
-        for (let k = mid + 1; k < j; k++) arr[k] = "L";
-      } else {
-        // even dots
-        for (let k = i + 1; k <= mid; k++) arr[k] = "R";
-        for (let k = mid + 1; k < j; k++) arr[k] = "L";
-      }
-    }
+    if (j > i) segmentModifier(arr, i, j);
     i = j;
   }
 
   // remove the added L and R at the beginning and end, and create a string
   return arr.slice(1, n - 1).join("");
 };
+
+function segmentModifier(arr, i, j) {
+  if (arr[i] === "L" && arr[j] === "L") {
+    // make all dots as L
+    for (let k = i + 1; k < j; k++) arr[k] = "L";
+  } else if (arr[i] === "R" && arr[j] === "R") {
+    // make all dots as R
+    for (let k = i + 1; k < j; k++) arr[k] = "R";
+  } else if (arr[i] === "L" && arr[j] === "R") {
+    // nothing to do
+  } else {
+    // solve acc. to odd and even number of dots
+    let mid = Math.floor((i + j) / 2);
+    if ((j - i) % 2 === 0) {
+      // odd dots
+      for (let k = i + 1; k < mid; k++) arr[k] = "R";
+      for (let k = mid + 1; k < j; k++) arr[k] = "L";
+    } else {
+      // even dots
+      for (let k = i + 1; k <= mid; k++) arr[k] = "R";
+      for (let k = mid + 1; k < j; k++) arr[k] = "L";
+    }
+  }
+}
