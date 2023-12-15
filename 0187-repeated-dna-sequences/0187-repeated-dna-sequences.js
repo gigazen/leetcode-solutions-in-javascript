4830ed75c046b5a480d76cfbec5506d79a74d7d2
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-12-15 21:51:14 +0530

    Time: 66 ms (92.97%), Space: 49.8 MB (96.26%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
@@ -2,12 +2,45 @@
  * @param {string} s
  * @return {string[]}
  */
-var findRepeatedDnaSequences = function (s) {
-    let set = new Set();
-    let result = new Set();
-    for (let i = 0; i <=s.length - 10; i++) {
-        let str = s.substring(i, i + 10) 
-        set.has(str) ? result.add(str) : set.add(str)
+function findRepeatedDnaSequences(s) {
+  // if s.length < 10 there can't be a 10 character sequence in it
+  if (s.length < 10) return [];
+  const charCode = {
+    A: 1,
+    C: 2,
+    G: 3,
+    T: 4,
+  };
+  const hashMap = new Map();
+  let rollingHash = 0;
+
+  // calculate the rolling hash for the first 9 characters
+  for (let i = 0; i < 9; i++) {
+    rollingHash = rollingHash * 5 + charCode[s[i]];
+  }
+
+  const result = [];
+
+  for (let i = 9; i < s.length; i++) {
+    // rolling hash for the 10 character window
+    // s[i - 9 : i]
+    rollingHash = rollingHash * 5 + charCode[s[i]];
+
+    if (!hashMap.has(rollingHash)) {
+      // if seeing hash the first time
+      // add it to result the next time you see it
+      hashMap.set(rollingHash, true);
+    } else if (hashMap.get(rollingHash)) {
+      // if seen hash once before add it to result
+      result.push(s.slice(i - 9, i + 1));
+      // and do not add it next time you see it
+      hashMap.set(rollingHash, false);
     }
-    return [...result]
-};
+
+    // rolling hash for the 9 character window
+    // s[i - 8 : i]
+    rollingHash -= charCode[s[i - 9]] * Math.pow(5, 9);
+  }
+
+  return result;
+}
