fd76397814977dacb2af3752215b8bb8f802a37f
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-12-15 20:19:56 +0530

    Time: 141 ms (11.87%), Space: 69.4 MB (11.21%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
@@ -2,19 +2,24 @@
  * @param {string} s
  * @return {string[]}
  */
-var findRepeatedDnaSequences = function (s) {
-  let map = new Map();
-  let resultArr = [];
-
-  for (let i = 0, j = 9; j < s.length; j++) {
-    let sequence = s.slice(i, j + 1);
-    map.set(sequence, 1 + (map.get(sequence) || 0));
-    i++;
+var findRepeatedDnaSequences = function(s) {
+  const sequenceMap = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+  const base = 4;
+  const hashMap = {};
+  let rollingHash = 0;
+  for(let i = 0, power = 9; i < 10; i++, power--) {
+    rollingHash += base ** power * sequenceMap[s[i]];
   }
-
-  for (let [key, value] of map.entries()) {
-    if (value > 1) resultArr.push(key);
+  hashMap[rollingHash] = s.slice(0, 10);
+  let answer = new Set();
+  for(let i = 10; i < s.length; i++) {
+    let nextRollingHash = base * (rollingHash - base ** 9 * sequenceMap[s[i - 10]]) + sequenceMap[s[i]];
+    if(nextRollingHash in hashMap) {
+      answer.add(hashMap[nextRollingHash])
+    } else {
+      hashMap[nextRollingHash] = s.slice(i - 9, i + 1);
+    }
+    rollingHash = nextRollingHash;
   }
-
-  return resultArr;
+  return [...answer.values()];
 };
