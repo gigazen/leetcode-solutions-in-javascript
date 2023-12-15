96c145e4309cfffecc83619b0381ad012865f11a
Deb J. Bhatt <debanjanb01234@gmail.com>
2023-12-15 20:22:32 +0530

    Time: 80 ms (65.27%), Space: 53 MB (92.97%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
@@ -2,31 +2,12 @@
  * @param {string} s
  * @return {string[]}
  */
-var findRepeatedDnaSequences = function(s) {
-    if (s.length < 10) {
-        return [];
+var findRepeatedDnaSequences = function (s) {
+    let set = new Set();
+    let result = new Set();
+    for (let i = 0; i <=s.length - 10; i++) {
+        let str = s.substring(i, i + 10) 
+        set.has(str) ? result.add(str) : set.add(str)
     }
-    let hashSet = new Set(), hash = 0, windowSize = 10, base = 4;
-    let decoded = {'A': 1, 'C': 2, 'G': 3, 'T': 4};
-    // process the first window separately
-    for (let i = 0; i < 10; i++) {
-        hash += Math.pow(base, windowSize-i-1) * decoded[s[i]];
-    }
-    hashSet.add(hash);
-    let res = new Set();
-    for (let i = 10; i < s.length; i++) {
-        // subtract the left-most bit
-        hash -= Math.pow(base, windowSize-1) * decoded[s[i-windowSize]];
-        hash *= base;
-        hash += decoded[s[i]];
-        if (hashSet.has(hash)) {
-            res.add(s.substring(i+1-windowSize, i+1));
-        } else {
-            hashSet.add(hash);
-        }
-    }
-    return Array.from(res);
-    // T.C: O(K*(N-K+1)) where K = 10 in the worst case 
-    // but average time complexity will be O(N-K+1) which is much better
-    // S.C: O(K*(N-K+1))
-}
+    return [...result]
+};
