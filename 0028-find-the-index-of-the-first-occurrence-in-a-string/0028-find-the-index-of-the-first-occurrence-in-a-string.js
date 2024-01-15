1ba57e0f9c837472609f086138dfb489904e6eb4
debanjanbe <debanjanb01234@gmail.com>
2024-01-16 00:22:38 +0530

    Time: 57 ms (34.17%), Space: 42.3 MB (25.64%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -5,17 +5,18 @@
  */
 var strStr = function (haystack, needle) {
   if (needle.length === 0) return 0;
-  if (haystack.length < needle.length) return -1;
-
-  let nIdx = 0;
+  if (haystack.length < needle.length) return -1;
+  
   for (let i = 0; i < haystack.length; i++) {
-    if (haystack[i] === needle[nIdx]) {
-      nIdx++;
-      if (nIdx === needle.length) return i - (nIdx - 1);
-    } else {
-      i = i - nIdx;
-      nIdx = 0;
+    let isMatch = true;
+    for (let j = 0; j < needle.length; j++) {
+      if (haystack[i + j] !== needle[j]) {
+        isMatch = false;
+        break;
+      }
     }
+
+    if (isMatch) return i;
   }
   return -1;
 };
