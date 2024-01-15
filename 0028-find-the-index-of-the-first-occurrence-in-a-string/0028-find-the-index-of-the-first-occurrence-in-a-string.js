be11eb4ed62e4e2683248fd545897b87bfa21d77
debanjanbe <debanjanb01234@gmail.com>
2024-01-16 00:20:42 +0530

    Time: 56 ms (38.25%), Space: 42.3 MB (20.02%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -3,53 +3,20 @@
  * @param {string} needle
  * @return {number}
  */
-function LPSArrForPattern(pat) {
-  let [patLength, pre, suf] = [pat.length, 0, 1];
-  if (patLength === 0) return 0;
-
-  let lpsArr = new Array(patLength).fill(0);
-
-  while (suf < patLength) {
-    // match
-    if (pat[pre] === pat[suf]) {
-      lpsArr[suf] = pre + 1;
-      pre++;
-      suf++;
-    } else {
-      // mismatch and pre at 0
-      if (pre === 0) {
-        lpsArr[suf] = 0;
-        suf++;
-      } else {
-        // mismatch and pre not at 0
-        pre = lpsArr[pre - 1];
+var strStr = function (haystack, needle) {
+  if (needle.length === 0) return 0;
+  if (haystack.length < needle.length) return -1;
+  
+  for (let i = 0; i < haystack.length; i++) {
+    let isMatch = true;
+    for (let j = 0; j < needle.length; j++) {
+      if (haystack[i + j] !== needle[j]) {
+        isMatch = false;
+        break;
       }
     }
+
+    if (isMatch) return i;
   }
-
-  return lpsArr;
-}
-
-var strStr = function (str, pat) {
-  let [strLength, patLength, first, second] = [str.length, pat.length, 0, 0];
-
-  while (first < strLength && second < patLength) {
-    // match
-    if (str[first] === pat[second]) {
-      first++;
-      second++;
-    } else {
-      // mismatch and pre at 0
-      if (second === 0) {
-        first++;
-      } else {
-        // mismatch and pre not at 0
-        let lpsArr = LPSArrForPattern(pat);
-        second = lpsArr[second - 1];
-      }
-    }
-  }
-
-  if (second === patLength) return first - second;
   return -1;
 };
